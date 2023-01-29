06c8b44657074f10c802cfcb58928fae3b75cda5
Ambika Kochuparambil Sidharthan <ak21ace@herts.ac.uk>
2023-01-29 21:47:39 +0000

    Updated with options

--- starter/index.js
+++ starter/index.js
@@ -5,6 +5,32 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    {
+        type:'input',
+        name:'name',
+        message:'Enter Project Name?',
+    },
+    {
+        type:'input',
+        name:'description',
+        message:'Enter Project Description?',
+    },
+    {
+        type:'input',
+        name:'contents',
+        message:'Enter table of contents?'
+    },
+    {
+        type:'input',
+        name:'install',
+        message:'Enter the command to install dependencies',
+        default: 'npm i',
+    },
+    {
+        type:'input',
+        name:'usage',
+        message:'Enter the usage of the Repo?'
+    }
 
 ];
 

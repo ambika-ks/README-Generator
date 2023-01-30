59fa62b5e5a155433d3808e36dab86ecbe924e82
Ambika Kochuparambil Sidharthan <ak21ace@herts.ac.uk>
2023-01-30 17:23:27 +0000

    Added Questions for the User

--- starter/index.js
+++ starter/index.js
@@ -17,20 +17,43 @@ const questions = [
     },
     {
         type:'input',
-        name:'contents',
-        message:'Enter table of contents?'
-    },
-    {
-        type:'input',
         name:'install',
         message:'Enter the command to install dependencies',
         default: 'npm i',
     },
     {
         type:'input',
+        name:'tests',
+        message:'Enter the command to run tests?',
+        default: 'npm test',
+    },
+    {
+        type:'input',
         name:'usage',
-        message:'Enter the usage of the Repo?'
-    }
+        message:'Enter the usage of the Repo : Run command ?',
+        default: 'node index.js'
+    },
+    {
+        type:'list',
+        name:'license',
+        message:'Enter your Project Licence?',
+        choices :['MIT','Boost','Apache','Mozilla','None'],
+    },
+    {
+        type:'input',
+        name:'contribute',
+        message:'Enter the Contributers of the project ',
+    },
+    {
+        type:'input',
+        name:'github',
+        message:'Enter GitHub Username?'
+    },
+    {
+        type:'input',
+        name:'email',
+        message:'Enter email address?'
+    },
 
 ];
 

7ba88c0d775001340c722338788352c90bce36dc
Ambika Kochuparambil Sidharthan <ak21ace@herts.ac.uk>
2023-01-30 17:29:22 +0000

    Added Table of contents in the readme.md in generateMarkdown.js

--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,7 +1,37 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
 
+# ${data.name}
+${LicenseBadge(answers.license)} 
+## Table of Content
+  - [Project Description](#description)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Installation](#installation)
+  - [Questions](#questions)
+  - [License](#license)
+
+## Description
+  ${answers.description}
+
+## Usage
+  Run the command " ${answers.usage} "and answer the prompted Questions
+
+## Installation
+  Command to install dependencies : ${answers.install}\n
+  Command to run tests : ${answers.tests}
+
+## Contributing
+  ${answers.contribute}
+
+## Questions
+  E-mail Address : ${answers.email}\n
+  GitHub Username: ${answers.github}
+
+## License
+  Licensed under the ${answers.license} License
+  
 `;
 }
 

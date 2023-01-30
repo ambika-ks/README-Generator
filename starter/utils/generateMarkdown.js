cfdc17d3f1ed66247ade38fdc87255d6f59ac98d
Ambika Kochuparambil Sidharthan <ak21ace@herts.ac.uk>
2023-01-30 17:30:55 +0000

    Added a function for License Badge

--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,3 +1,15 @@
+
+function LicenseBadge(License){
+  const badges={
+    Apache:'[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
+    MIT:'[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+    Boost: '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)',
+    Mozilla: '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
+  }
+  return badges[License]
+}
+
+
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `
